617b70d2354f83fa522334943e020a6fbd590b2c
Thues021 <mateusoliver2109@hotmail.com>
2022-08-16 13:01:18 -0300

    feature/Url_com_OkHttpClient

--- app/src/main/java/daniel/lop/io/marvelappstarter/util/Constant.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constant.kt
@@ -7,4 +7,8 @@ object Constants {
     const val PUPLIC_KEY = "372e6759431a413c427c4dc396ab77c2"
     const val PRIVATE_KEY = "56611d52828a716d195f1c5f409cc04d462be641"
 
+    const val APIKEY = "apikey"
+    const val HASH = "hase"
+    const val TS = "ts"
+
 }
