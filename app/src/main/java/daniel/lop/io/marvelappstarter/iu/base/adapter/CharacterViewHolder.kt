0a5dafbde3bd9b500c7e990a6dc96852930db3ff
Thues021 <mateusoliver2109@hotmail.com>
2022-08-17 10:23:20 -0300

    feature/Criando_Adapter_e_Extension

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/iu/base/adapter/CharacterViewHolder.kt
@@ -0,0 +1,83 @@
+package daniel.lop.io.marvelappstarter.iu.base.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import daniel.lop.io.marvelappstarter.R
+import daniel.lop.io.marvelappstarter.databinding.ItemCharacterBinding
+import daniel.lop.io.marvelappstarter.model.CaracterModel
+import daniel.lop.io.marvelappstarter.util.limitDescription
+
+
+class CharacterAdapter: RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder>() {
+
+    inner class CharacterViewHolder(val binding: ItemCharacterBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    private val differCallBack = object: DiffUtil.ItemCallback<CaracterModel>(){
+
+        override fun areItemsTheSame(oldItem: CaracterModel, newItem: CaracterModel): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+
+        override fun areContentsTheSame(oldItem: CaracterModel, newItem: CaracterModel): Boolean {
+            return  oldItem.id == newItem.id && oldItem.name == newItem.name &&
+                    oldItem.descripton == newItem.descripton &&
+                    oldItem.thumbnailModel.patch == newItem.thumbnailModel.patch &&
+                    oldItem.thumbnailModel.extension == newItem.thumbnailModel.extension
+
+        }
+
+    }
+
+    private val difer = AsyncListDiffer(this, differCallBack )
+
+    var characters: List<CaracterModel>
+    get()= difer.currentList
+    set(value)= difer.submitList(value)
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
+        return CharacterViewHolder(
+            ItemCharacterBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = characters.size
+
+    override fun onBindViewHolder(holder: CharacterViewHolder, position: Int) {
+
+        val character = characters[position]
+            holder.binding.apply {
+                tvNameCharacter.text = character.name
+                if (character.descripton == "") {
+                    tvNameCharacter.text =
+                    holder.itemView.context.getString(R.string.text_description_empty)
+                }else{
+                    tvDescriptionCharacter.text = character.descripton.limitDescription(100)
+                }
+                Glide.with(holder.itemView.context)
+                    .load(character.thumbnailModel.patch + "." + character.thumbnailModel.extension)
+                    .into(imgCharacter)
+            }
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.let {
+                it(character)
+            }
+        }
+    }
+
+    private var onItemClickListener:((CaracterModel) -> Unit)? = null
+
+    fun setOnClickListener(listener: (CaracterModel) -> Unit){
+            onItemClickListener = listener
+    }
+
+    }
