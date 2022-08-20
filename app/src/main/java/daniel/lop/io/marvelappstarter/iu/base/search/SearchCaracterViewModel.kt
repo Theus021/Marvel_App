75e78adfd8e9c6e88d48845d9422841ad1ae5298
Thues021 <mateusoliver2109@hotmail.com>
2022-08-20 18:33:01 -0300

    feature/search_list_view_model

--- app/src/main/java/daniel/lop/io/marvelappstarter/iu/base/search/SearchCaracterViewModel.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/iu/base/search/SearchCaracterViewModel.kt
@@ -1,6 +1,51 @@
 package daniel.lop.io.marvelappstarter.iu.base.search
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import daniel.lop.io.marvelappstarter.model.CaracterModelResponse
+import daniel.lop.io.marvelappstarter.repository.MarvelRepository
+import daniel.lop.io.marvelappstarter.state.ResourceState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import retrofit2.Response
+import java.io.IOException
+import javax.inject.Inject
 
-class SearchCaracterViewModel: ViewModel() {
+@HiltViewModel
+class SearchCaracterViewModel @Inject constructor(
+    private val repository: MarvelRepository
+) : ViewModel() {
+
+    private val _searchCharacter =
+        MutableStateFlow<ResourceState<CaracterModelResponse>>(ResourceState.Empty())
+    val searchCharacter: StateFlow<ResourceState<CaracterModelResponse>> = _searchCharacter
+
+    fun fetch(nameStartsWith: String) = viewModelScope.launch {
+        safeFetch(nameStartsWith)
+    }
+
+    private suspend fun safeFetch(nameStartsWith: String) {
+        _searchCharacter.value = ResourceState.Loading()
+        try {
+            val response = repository.list(nameStartsWith)
+            _searchCharacter.value = handleResponse(response)
+        }catch (t: Throwable){
+            when(t){
+                is IOException -> _searchCharacter.value = ResourceState.Error("Error na Rede")
+                else -> _searchCharacter.value = ResourceState.Error("Error da Conversão de dados")
+            }
+        }
+    }
+
+    private fun handleResponse(response: Response<CaracterModelResponse>): ResourceState<CaracterModelResponse> {
+        if (response.isSuccessful) {
+            response.body()?.let { values ->
+                return ResourceState.Success(values)
+
+            }
+        }
+        return ResourceState.Error(response.message())
+    }
 }
